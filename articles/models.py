6f279a71205ac7e66c30695920d396b1aeb023b0
Victor Glebov <glebovv3105@gmail.com>
2020-10-20 16:02:29 -0600

    Added models for sections and a relationship class

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tag = models.ManyToManyField('Section', through='AssignedTags', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +15,17 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Раздел')
+    main = models.BooleanField()
+
+    def __str__(self):
+        main_str = 'main' if self.main else 'secondary'
+        return f'{self.name}, {main_str}'
+
+
+class AssignedTags(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles.apps.ArticlesConfig'
+    # 'articles.apps.ArticlesConfig',
+    'articles'
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,8 @@ DATABASES = {
         'NAME': 'netology_m2m_relations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
 
