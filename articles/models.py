d78f5ee18fcb7f81dfe3eac923976cf5babb6b0d
Victor Glebov <glebovv3105@gmail.com>
2020-10-20 21:46:24 -0600

    Optimized way of extracting scopes for an article

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    tag = models.ManyToManyField('Scope', through='AssignedTags', null=True, blank=True)
+    scopes = models.ManyToManyField('Scope', through='AssignedTags', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Статья'
